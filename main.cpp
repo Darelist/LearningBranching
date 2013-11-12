1952ca52bc37064553a0cd462311c46fd50eb296
Darelis <darelist@gmail.com>
2013-11-12 19:19:15 +0200

    papildyta lt versija ir pridejau for cikla

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 using namespace std;
 int main(){
-    cout << "Hello World!" << endl;
+    for(int i=0; i<10; i++)
+    {
+        cout << "Hello World!" << endl;
+        cout << "Sveikas pasauli!" << endl;
+    }
     return 0;
 }
